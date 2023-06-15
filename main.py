e88cc229efdaded90e1cdfcc1c327d40fe45074d
Zaid <itsunknown122@gmail.com>
2023-06-15 08:26:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -218,6 +218,16 @@ t1.forward(260)
 t1.end_fill()
 #At this point, we have completed drawing Radhe Krishna
 
+
+t1.pu()
+t1.right(100)
+t1.forward(115)
+t1.right(85)
+t1.forward(450)
+
+#Lets also write their holy name in our drawing 
+t1.color("#00a606")
+t1.write("Radha Krishna - Preetam ", font=("Harlow Solid Italic",45, "bold"))
 t1.hideturtle()
 
 turtle.done()
