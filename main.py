6f74a2ea1ccb9da35303011642f415a2a0cad471
Zaid <itsunknown122@gmail.com>
2023-06-15 10:49:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ def outline():
                                 thresholdType=cv2.THRESH_BINARY, blockSize=9, C=2)
     return th3
 
-image = 'maxresdefault.jpg'
+image = 'IMG_20230615_104810_533.jpg'
 im = cv2.imread(image, 0)
 th3 = outline()
 
