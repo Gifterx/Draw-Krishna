a5eb1c4caa692d343d74dd91d895a638e003f98f
Zaid <itsunknown122@gmail.com>
2023-06-15 09:58:05 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,60 +1,71 @@
+from svgpathtools import svg2paths2, Path, Line
+from tkinter import Tk, filedialog
 import turtle
-import xml.etree.ElementTree as ET
 
-def draw_svg(svg_file):
-    # Create a turtle instance
-    screen = turtle.Screen()
-    t = turtle.Turtle()
+root = Tk()
+root.withdraw()
 
-    # Parse the SVG file
-    tree = ET.parse(svg_file)
-    root = tree.getroot()
+open_file = filedialog.askopenfilenames(filetypes=[("imagebyzaid", ".svg")])
+file = open_file[0]
+paths, attributes, svg_attributes = svg2paths2(file)
 
-    # Set initial position and color
-    t.penup()
-    t.goto(0, 0)
-    t.pendown()
-    t.pensize(2)
-    t.speed(3)
+if len(paths) == 1:
+    exit()
 
-    # Iterate through SVG elements
-    for element in root.iter():
-        if element.tag == '{http://www.w3.org/2000/svg}path':
-            # Handle path commands
-            path_data = element.get('d')
-            commands = parse_path_commands(path_data)
+turtle.setworldcoordinates(0, 0, 500, 500)
 
-            for command in commands:
-                if command[0] == 'M':
-                    x, y = command[1:]
-                    t.penup()
-                    t.goto(x, -y)
-                    t.pendown()
-                elif command[0] == 'L':
-                    x, y = command[1:]
-                    t.goto(x, -y)
 
-    # Close the turtle graphics
-    screen.exitonclick()
+def Vector(pathB):
+    X1 = pathB[0].real
+    Y1 = pathB[0].imag
 
-def parse_path_commands(path_data):
-    commands = []
-    current_command = ''
-    command_data = []
+    X2 = pathB[1].real
+    Y2 = pathB[1].imag
 
-    for char in path_data:
-        if char.isalpha():
-            if current_command:
-                commands.append((current_command, command_data))
-                command_data = []
-            current_command = char
-        elif char.isdigit() or char in ('.'):
-            command_data.append(float(char))
+    turtle.goto(X1,Y1)
+    turtle.pendown()
+    turtle.goto(X2,Y2)
+    turtle.penup()
 
-    if current_command:
-        commands.append((current_command, command_data))
+def Linear(x, y , t):
+    return (x*(1-t)) + (y*t)
 
-    return commands
+def Linear2(x, y,t):
+    return Linear(x[0], y[0], t), Linear(x[1],y[1],t)
 
-# Usage
-draw_svg('imagebyzaid.svg')
+def Bezier(P0,P1,P2,P3, t):
+    c1 = Linear2(P1,P2,t)
+    c2 = Linear2(P0,c1,t)
+    c3 = Linear2(c2, P3, t)
+    c4 = Linear2(c2, c3, t)
+
+    return c4
+
+def BezierTurtle(P0,P1,P2,P3):
+    t = 0
+    while t < 1:
+        t+= 0.001
+        x,y = Bezier(P0,P1,P2,P3,t)
+        if turtle.distance(x,y) >= 1:
+            turtle.goto(x,y)
+            turtle.pendown()
+
+
+
+for l in range(len(paths)):
+    bluepath = paths[l]
+    for i in range(len(bluepath)):
+        print(turtle.pos())
+        turtle.penup()
+        if isinstance(bluepath[i],Line):
+            Vector(bluepath[i])
+        else:
+            P0 = (bluepath[i][0].real, bluepath[i][0].imag)
+            P1 = (bluepath[i][1].real, bluepath[i][1].imag)
+            P2 = (bluepath[i][2].real, bluepath[i][2].imag)
+            P3 = (bluepath[i][3].real, bluepath[i][3].imag)
+            BezierTurtle(P0,P1,P2,P3)
+            turtle.penup()
+
+
+turtle.done()
