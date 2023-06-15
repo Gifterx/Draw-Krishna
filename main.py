2ec3efdd86447fdb3f159689e6c689622ea82073
Zaid <itsunknown122@gmail.com>
2023-06-15 10:05:20 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,71 +1,89 @@
-from svgpathtools import svg2paths2, Path, Line
-from tkinter import Tk, filedialog
-import turtle
+import argparse
+import math
+from turtle import Screen, Turtle
+from typing import Optional
 
-root = Tk()
-root.withdraw()
+from svgpathtools import svg2paths
 
-open_file = filedialog.askopenfilenames(filetypes=[("imagebyzaid", ".svg")])
-file = open_file[0]
-paths, attributes, svg_attributes = svg2paths2(file)
-
-if len(paths) == 1:
-    exit()
-
-turtle.setworldcoordinates(0, 0, 500, 500)
+turtle = Turtle()
+screen = Screen()
 
 
-def Vector(pathB):
-    X1 = pathB[0].real
-    Y1 = pathB[0].imag
-
-    X2 = pathB[1].real
-    Y2 = pathB[1].imag
-
-    turtle.goto(X1,Y1)
-    turtle.pendown()
-    turtle.goto(X2,Y2)
-    turtle.penup()
-
-def Linear(x, y , t):
-    return (x*(1-t)) + (y*t)
-
-def Linear2(x, y,t):
-    return Linear(x[0], y[0], t), Linear(x[1],y[1],t)
-
-def Bezier(P0,P1,P2,P3, t):
-    c1 = Linear2(P1,P2,t)
-    c2 = Linear2(P0,c1,t)
-    c3 = Linear2(c2, P3, t)
-    c4 = Linear2(c2, c3, t)
-
-    return c4
-
-def BezierTurtle(P0,P1,P2,P3):
-    t = 0
-    while t < 1:
-        t+= 0.001
-        x,y = Bezier(P0,P1,P2,P3,t)
-        if turtle.distance(x,y) >= 1:
-            turtle.goto(x,y)
-            turtle.pendown()
+def parse_styles(attrs: list[dict]) -> list[dict]:
+    for dictionary in attrs:
+        try:
+            pairs = dictionary['style'].replace(' ', '')
+        except KeyError:
+            continue
+        pairs = pairs.rstrip(';').split(';')
+        for pair in pairs:
+            key, value = pair.split(':')
+            if value == 'none':
+                value = None
+            dictionary[key] = value
+    return attrs
 
 
-
-for l in range(len(paths)):
-    bluepath = paths[l]
-    for i in range(len(bluepath)):
-        print(turtle.pos())
-        turtle.penup()
-        if isinstance(bluepath[i],Line):
-            Vector(bluepath[i])
-        else:
-            P0 = (bluepath[i][0].real, bluepath[i][0].imag)
-            P1 = (bluepath[i][1].real, bluepath[i][1].imag)
-            P2 = (bluepath[i][2].real, bluepath[i][2].imag)
-            P3 = (bluepath[i][3].real, bluepath[i][3].imag)
-            BezierTurtle(P0,P1,P2,P3)
-            turtle.penup()
+def parse_paths(paths: list, quality: int = 8, offset: tuple[float, float] = (0, 0)) -> list[list]:
+    new_paths = []
+    for path in paths:
+        new_path = []
+        for subpaths in path.continuous_subpaths():
+            points = []
+            for segment in subpaths:
+                interp_num = math.ceil(segment.length() / quality)
+                positions = [x / interp_num for x in range(interp_num)]
+                points.extend([segment.point(position) for position in positions])
+            new_path.append([(point.real + offset[0], -point.imag - offset[1]) for point in points])
+        new_paths.append(new_path)
+    return new_paths
 
 
-turtle.done()
+def move_to(coords: tuple[float, float], draw: bool = True):
+    wasdown = turtle.isdown()
+    turtle.pen(pendown=draw)
+    turtle.goto(coords[0], coords[1])
+    turtle.pen(pendown=wasdown)
+
+
+def draw_path(path, color: Optional[str] = None, fill: Optional[str] = None):
+    if color:
+        turtle.color(color)
+    for segment in path:
+        move_to(segment[0], False)
+        if fill:
+            turtle.color(fill)
+            turtle.begin_fill()
+        for point in segment[1:]:
+            move_to(point, True)
+        if fill:
+            turtle.end_fill()
+
+
+def main(file_path: str, loop: bool = False, quality: int = 1, n: int = 0):  # pylint: disable=invalid-name
+    paths, attrs, svg_attrs = svg2paths(file_path, return_svg_attributes=True)  # type: ignore
+    attrs = parse_styles(attrs)
+    width, height = int(float(svg_attrs['width'])), int(float(svg_attrs['height']))
+    offset = (-width / 2, -height / 2)
+    paths = parse_paths(paths, quality, offset)
+    screen.tracer(n=n, delay=0)
+    screen.screensize(width, height)
+    while True:
+        turtle.reset()
+        turtle.hideturtle()
+        for path, attr in zip(paths, attrs):
+            draw_path(path, attr.get('stroke'), attr.get('fill'))
+        if not loop:
+            break
+    screen.update()
+    screen.mainloop()
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('path')
+    parser.add_argument('-l', '--loop', action='store_true')
+    parser.add_argument('-q', '--quality', type=int, default=1)
+    parser.add_argument('-n', type=int, default=0)
+    args = parser.parse_args()
+    main(args.path, args.loop, args.quality, args.n)
