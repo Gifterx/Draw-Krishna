f9a46c6ddeb15fc764157e363087ef0c97d1aa58
Zaid <itsunknown122@gmail.com>
2023-06-15 08:54:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
+
 import turtle as tu
-import cv2
 from svgpathtools import svg2paths2
 from svg.path import parse_path
 from tqdm import tqdm
+
 class sketch_from_svg:
 
     def __init__(self,path,scale=30,x_offset=350,y_offset=350):
@@ -14,6 +15,7 @@ class sketch_from_svg:
 
     def hex_to_rgb(self,string):
         strlen = len(string)
+
         if string.startswith('#'):
             if strlen == 7:
                 r = string[1:3]
@@ -34,10 +36,8 @@ class sketch_from_svg:
         
         return int(r,16)/255,int(g,16)/255, int(b,16)/255
 
-    
-
     def load_svg(self):
-        print('loading data')
+        print('Loading Data - Please Wait Few Seconds')
         paths,attributes,svg_att = svg2paths2(self.path)
         h = svg_att["height"]
         w = svg_att['width']
@@ -48,21 +48,20 @@ class sketch_from_svg:
         for i in tqdm(attributes):
             path = parse_path(i['d'])
             co = i['fill']
-            #print(co)
             col = self.hex_to_rgb(co)
-            #print(col)
+
             n = len(list(path))+2       
             pts = [((int((p.real/self.width)*self.scale))-self.x_offset, (int((p.imag/self.height)*self.scale))-self.y_offset) for p in (path.point(i/n) for i in range(0,n+1))]
             res.append((pts,col))
-            #res.append(pts)
-        print('svg data loaded')
+            tu.title("Made By CodedWithAjay")
+
+        print('SVG Data Loaded')
         return res
 
     def move_to(self,x, y):
         self.pen.up()
         self.pen.goto(x,y)
         self.pen.down()
-
 
     def draw(self,retain=True):
         coordinates = self.load_svg()
@@ -71,27 +70,33 @@ class sketch_from_svg:
         for path_col in coordinates:
             f = 1
             self.pen.color('black')
-            #print(path_col)
+
             path = path_col[0]
-            #print(path_col)
             col = path_col[1]
-            #print(col)
+
             self.pen.color(col)
             self.pen.begin_fill()
-            next = 0
+
             for coord in path:
-                #for coord in path_col:
                 x,y = coord
                 y *= -1
-                #print(x,y)
+
                 if f:
                     self.move_to(x, y)
                     f=0
                 else:
                     self.pen.goto(x,y)
             self.pen.end_fill()
+            writesomething()
 
         if retain == True:
             tu.done()
-pen= sketch_from_svg('hanumanji.svg',scale=70)  
+
+def writesomething():
+    tu.goto(250,100)
+    tu.write("@PyZaid",font=("italian", "30", "bold"))
+    tu.goto(210,50)
+    tu.write("🙏 Jay Shree Ram 🙏",font=("italian", "30", "bold"))
+
+pen= sketch_from_svg('2022_07_17.svg',scale=70)
 pen.draw()
